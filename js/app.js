9a00d998ce87e5aaa45ab25d672eed4967b348c0
Dinh Huy <huy.truong@py-media.com>
2024-05-31 01:22:18 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function($) {
+    $('.list-logo').slick({
+        autoplay: true,
+        slidesToShow: 10,
+        infinite: true, 
+        speed: 2000, 
+        autoplaySpeed: 0, 
+        cssEase: 'linear', 
+        variableWidth: true, 
+        arrows: false,
+        pauseOnHover: false ,
+    });
+    
+})
+
